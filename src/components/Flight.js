562a4f327f4d210d1cbb57f41d51e9a8443951ab
Adrien Demarez <swayndev@gmail.com>
2022-03-11 15:47:56 +0100

    search flight by departure city, arrival city and filter criteria

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -11,16 +11,22 @@ function Flight ({flightKey}) {
     <div>
     
         <h2>{flightKey.cityFrom} -> {flightKey.cityTo}</h2>
-        <br/>
         <h3>
-        {DateTime.fromMillis(flightKey.dTime * 1000).toFormat('hh:mm')} -> {DateTime.fromMillis(flightKey.aTime * 1000).toFormat('hh:mm')}
+        {DateTime.fromMillis(flightKey.dTime * 1000).toFormat('DD')} => {DateTime.fromMillis(flightKey.aTime * 1000).toFormat('DD')}
+        </h3>
+
+           <h3>
+        {DateTime.fromMillis(flightKey.dTime * 1000).toFormat('T')} => {DateTime.fromMillis(flightKey.aTime * 1000).toFormat('T')}
         </h3>
         
-        <h3>Duration: {DateTime.fromMillis(flightKey.duration.total * 1000).toFormat('hh:mm')} hr </h3>
-        <h3>Price: {flightKey.price} EUR </h3>
-        <br/>
         
-        <p>Number of stop over with this flight: {flightKey.route.length -1} </p>
+        <h3>Duration: {flightKey.fly_duration} </h3>
+        <h3>Price: {flightKey.price} EUR </h3>
+        
+        <h4>Stop overs: {flightKey.route.length -1} </h4>
+        <br/>
+        <div> --- </div>
+        <br/>
     </div>
 )
 }
--- src/components/Flights.js
+++ src/components/Flights.js
@@ -5,30 +5,47 @@ import {Link, useParams } from 'react-router-dom';
 
 function Flights() {     //{departureCity, arrivalCity}
 
-    const [flights, setFlights] = useState([])
+    // STATES AND PARAMS
+    const [offset, setOffset] = useState(0)
+    const [limit, setLimit] = useState(5)
+    
 
     const { dep, arr, crit } = useParams();
-
-
-    const url = `https://api.skypicker.com/flights?fly_from=${dep}&fly_to=${arr}&sort=${crit}&partner=data4youcbp202106&limit=5`;
+    
+    
+    const [flights, setFlights] = useState([])
     const [loading, setLoading ] = useState(false);
-
+    
+    const url = `https://api.skypicker.com/flights?fly_from=${dep}&fly_to=${arr}&sort=${crit}&partner=data4youcbp202106&offset=${offset}&limit=${limit}`;  // &offset=10
+    
     async function getData() {
         const resp =await fetch(url);
         const result = await resp.json();
-        result && setFlights(result.data)
+        result && result.data && setFlights(result.data)
         setLoading(true)
     }
-
-
+    
+    console.log('this is API results value', flights);
+    
     useEffect(()=> {
         getData();
         }, []
     )
-console.log('this is flights value', flights);
 
 
+// FUNCTIONS AND LOGIC
+const displayNext = () => {
+    
+    setOffset(offset + 5);
+    setLimit(offset + 5);
 
+    
+}
+
+const displayPrevious = () => {
+    const n = -5;
+    setOffset(offset + n) && setLimit(offset + n);
+}
 
 
     return (
@@ -38,7 +55,8 @@ console.log('this is flights value', flights);
     <br/>
 
     {!loading ? <div>
-    <p>Loading</p><p> ローディング　しばらくお待ちください。</p></div>:
+    <p><strong>Loading</strong></p>
+    <p> ローディング　しばらくお待ちください。</p></div>:
     flights.map((flight, index) => {
       return (
       <Flight key={index} flightKey={flight}/>
